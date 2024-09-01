c02018ff3c8c32f23fae6191f4d303e5f76757fb
likhith-pasupuleti <143747986+likhith-pasupuleti@users.noreply.github.com>
2024-09-01 07:41:03 -0700

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyCTYgiTJlDGi8Lb_2l2yy7GTy6jheogl5U";
+const GOOGLE_API_KEY = "";
 export const YOUTUBE_GOOGLE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50&chart=mostPopular&regionCode=IN&key=" +
   GOOGLE_API_KEY;
